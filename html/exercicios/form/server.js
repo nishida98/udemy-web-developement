f799a432f1848464bc06ca6421e3594bae99f912
Leonardo Nishida <leonardo.nishida@provider-it.com.br>
2022-09-21 17:25:12 -0300

    images

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended : true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('OK')
+    resp.send('Usuario incluido')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('Usuario alterado')
 })
 
 app.listen(3003)
